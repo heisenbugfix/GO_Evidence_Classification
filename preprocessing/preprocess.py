90fb1fd1155859e40882608c48cfd8c82c201be8
ashish <ashish.ranjan2000@gmail.com>
2018-03-17 11:57:24 -0400

    added code for text cleaning and tdfidf creation

--- preprocessing/preprocess.py
+++ preprocessing/preprocess.py
@@ -2,6 +2,11 @@ import pickle as pkl
 import obonet
 import json
 import numpy as np
+import re
+import string
+from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
+from nltk.stem.porter import PorterStemmer
 from sklearn.feature_extraction.text import TfidfTransformer, CountVectorizer
 from sklearn.linear_model.logistic import LogisticRegression
 from sklearn.preprocessing import OneHotEncoder
@@ -14,6 +19,7 @@ labels = ["DB","DB_OID","DB_OBS",
           "Annotation_EXT","Gene_PFID"]
 
 feature_labels = {"GO_ID", "DB_REF", "GO_PARENTS", "Aspect", "Taxon", "DB_OBSYN", "WITH"}
+regex = re.compile('[%s]' % re.escape(string.punctuation))
 
 def load_pkl_data(filename):
     f = open(filename, 'rb')
@@ -51,7 +57,7 @@ def load_pubmed_text_data(filename=None):
             pubid = splitted[0]
             pubyr = splitted[1]
             abstract = splitted[2]
-            data[pubid] = [{"date": pubyr, "abstract": abstract}]
+            data[pubid] = {"date": pubyr, "abstract": abstract}
     return data
 
 def dump_pubmed_json_fromtext(infile=None, outfile=None):
@@ -61,11 +67,9 @@ def dump_pubmed_json_fromtext(infile=None, outfile=None):
         with open(outfile, 'w') as f:
             json.dump(data,f, indent=2)
 
-def temp():
-    corpus = ["This is very strange palce in a strange land for it to be true with building car ghost town hood jacket",
-              "This is very nice"]
+def create_tfidf_data(text_list):
     cvec = CountVectorizer()
-    Y = cvec.fit_transform(corpus)
+    Y = cvec.fit_transform(text_list)
     tf_transformer = TfidfTransformer()
     X_train_tfidf = tf_transformer.fit_transform(Y)
     a = np.asarray(X_train_tfidf[1].todense()).flatten()
@@ -103,5 +107,51 @@ def get_parent_nodes(node, graph=None):
     return ans
 
 
+def create_training_data(data_filename, ontology_filename=None):
+    node_to_index, index_to_node = create_go_term_vector(ontology_filename)
+    json_data = load_json_data(data_filename)
+    lab = {"GO_ID", "DB_REF", "GO_PARENTS", "Aspect", "Taxon", "DB_OBSYN", "WITH"}
+    for point in json_data:
+        pass
 
 
+def collect_cleaned_goref_pubmed_data():
+    with open("../data/GO_REF.pickle", 'rb') as f:
+        gorefData = pkl.load(f)
+    all_text = []
+    for i, each in enumerate(gorefData):
+        if i > 19:
+            break
+        text = gorefData[each]
+        for line in text:
+            all_text.append(clean_text(line))
+    # pubmed_data = load_pubmed_text_data()
+    # for each in pubmed_data:
+    #     all_text.append(clean_text(pubmed_data[each]["abstract"]))
+    return all_text
+
+
+def clean_text(line, stemming=False):
+    line = line.lower()
+    # remove urls (courtesy : stackoverflow)
+    line = re.sub(
+        r'(?i)\b((?:https?://|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}     /)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:\'".,<>?«»“”‘’]))',
+        '', line)
+    new_line = []
+    # tokenizing
+    tokenized_line = word_tokenize(text=line)
+    for token in tokenized_line:
+        # removing punctuation and stop words
+        if '-' in token and len(token) > 3:
+            token = token.split('-')
+            new_token = ' '.join(token)
+        else:
+            new_token = regex.sub(u'', token)
+        if not new_token == u'' and not new_token in stopwords.words('english'):
+            if stemming:
+                new_token = PorterStemmer.stem(new_token)
+            new_line.append(new_token)
+    return ' '.join(new_line)
+
+text_list = collect_cleaned_goref_pubmed_data()
+create_tfidf_data(text_list)
